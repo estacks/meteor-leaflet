4ce565645721d91d05a877ffc5d7df78f3fed434
estacks <eric@king.rocks>
2016-11-02 21:16:46 -0400

    Fixed package

--- package.js
+++ package.js
@@ -3,7 +3,7 @@ Package.describe({
   summary: "AtmosphereJS wrapper for Leaflet.",
   git: "https://github.com/estacks/meteor-leaflet.git",
   author: "Eric King <eric@king.rocks>",
-  version: "3.0.1_1",
+  version: "3.0.1_3",
   license: "MIT"
 });
 
@@ -16,17 +16,17 @@ Npm.depends({
 Package.onUse(function (api) {
   api.versionsFrom('1.2');
   api.addFiles([
-    '.npm/package/node_modules/leaflet/src/leaflet-src.js',
+    '.npm/package/node_modules/leaflet/dist/leaflet-src.js',
     '.npm/package/node_modules/leaflet-providers/leaflet-providers.js',
     '.npm/package/node_modules/spin.js/spin.js',
     'lib/leaflet_spin.js',
     'styles/leaflet.css',
   ], 'client');
   api.addAssets([
-    'styles/images/layers-2x.png',
-    'styles/images/layers.png',
-    'styles/images/marker-icon-2x.png',
-    'styles/images/marker-icon.png',
-    'styles/images/marker-shadow.png'
+    'images/layers-2x.png',
+    'images/layers.png',
+    'images/marker-icon-2x.png',
+    'images/marker-icon.png',
+    'images/marker-shadow.png'
   ],'client');
 });
